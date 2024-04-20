c083f2ffd694b40a07d47d18ff5e71c6b33ff302
Praisejnr <122161276+Praisejnr@users.noreply.github.com>
2024-04-20 11:30:31 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function toggleBtn() {
+  const hamBtn = document.querySelector('.hamburger');
+  const navBar = document.querySelector('.navBar');
+
+  hamBtn.classList.toggle('active');
+  navBar.classList.toggle('active');
+}
+
+
+const images = [
+  '/img/background.jpg',
+  '/img/background1.jpg',
+  '/img/background2.jpg',
+  '/img/background3.jpg',
+  '/img/background4.jpg',
+  ];
+
+function changeBackground() {
+  const home = document.getElementById('home');
+  const randomizer = Math.floor(Math.random() * images.length);
+
+  home.style.backgroundImage = `url('${images[randomizer]}')`;
+}
+
+setInterval(changeBackground, 10000);
+
+changeBackground();
